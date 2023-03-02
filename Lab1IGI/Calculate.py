f14d61cb637431c3ef0f08f9755b22a8a00eb90c
Artur <artur.kudlasewitsch@gmail.com>
2023-03-02 23:37:37 +0300

    add Calculate

---
+++ Lab1IGI/Calculate.py
@@ -0,0 +1,11 @@
+def sum2(a, b, operation):
+        if operation == "add":
+            return a + b
+        elif operation == "sub":
+            return a - b
+        elif operation == "mult":
+            return a * b
+        elif operation == "div":
+            return a / b
+        else:
+            return None
