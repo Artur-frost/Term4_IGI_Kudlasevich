fa99fbdc9a082c7f6c048b3e7b34055c2f4c12bb
Artur <artur.kudlasewitsch@gmail.com>
2023-03-02 23:46:23 +0300

    add main

---
+++ Lab1IGI/main.py
@@ -0,0 +1,9 @@
+from Calculate import sum2
+
+from List import parse_even
+
+print("Hello, World!")
+
+print(parse_even([3, 7, 9, 4, 10, 13, 15, 17, 14, 20, 0, 1, 6, 4, 5, 2, 8, 11, 16, 12, 19, 18]))
+
+print(sum2(2, 3, "add"))
