05ee21b115d74b89afc23a07ae758c27b6136e73
Artur <artur.kudlasewitsch@gmail.com>
2023-03-07 17:00:10 +0300

    Fixed modules

--- Lab1IGI/Calculate.py
+++ Lab1IGI/Calculate.py
@@ -1,11 +1,31 @@
-def sum2(a, b, operation):
-        if operation == "add":
-            return a + b
-        elif operation == "sub":
-            return a - b
-        elif operation == "mult":
-            return a * b
-        elif operation == "div":
-            return a / b
+def function(number_1, number_2, op):
+    try:
+        number_1 = float(number_1)
+        number_2 = float(number_2)
+
+        if op == "add":
+            print(f"{number_1} + {number_2} = {number_1 + number_2}")
+            return False
+        elif op == "sub":
+            print(f"{number_1} - {number_2} = {number_1 - number_2}")
+            return False
+        elif op == "mul":
+            print(f"{number_1} * {number_2} = {number_1 * number_2}")
+            return False
+        elif op == "div":
+            if number_2 == 0:
+                print("Divide by zero")
+                return True
+            print(f"{number_1} / {number_2} = {number_1 / number_2}")
+            return False
         else:
-            return None
+            print("Incorrect operation")
+            return True
+    except ValueError:
+        return
+
+
+first_number = input("Enter the first number \t")
+second_number = input("Enter the second umber \t")
+action = input("Enter an action \t")
+print(function(first_number, second_number, action))
