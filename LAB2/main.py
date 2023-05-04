98c41f81cd63ab05c86b333a525b8ebf8062ec37
Artur <artur.kudlasewitsch@gmail.com>
2023-05-04 11:47:58 +0300

    add main

---
+++ LAB2/main.py
@@ -0,0 +1,13 @@
+from Task1.Main1 import task1_main
+from Task2.Constants2 import COMMANDS
+from Task2.Main2 import task2_main
+
+
+def main():
+
+    task1_main()
+    task2_main()
+
+
+if __name__ == "__main__":
+    main()
