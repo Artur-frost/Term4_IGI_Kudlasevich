9ef4a3a1e508053f4ec0f49d8934b0daa6f78148
Artur <artur.kudlasewitsch@gmail.com>
2023-09-15 02:41:19 +0300

    add urls

--- Lab4/myshop/shop/urls.py
+++ Lab4/myshop/shop/urls.py
@@ -4,7 +4,15 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('', views.home, name='product_list'),
+    path('articles/<int:article_id>/', views.article_detail, name='article-detail'),
+    path('store/', views.product_list, name='product_list'),
+    path('contacts/', views.contact_list, name='contact-list'),
+    path('privacy-policy/', views.privacy_policy, name='privacy-policy'),
+
+    path('partners/', views.partner_list, name='partner-list'),
+    path('store/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('about/', views.company_info, name='company-info'),
+    path('articles/', views.article_list, name='article-list'),
 ]
