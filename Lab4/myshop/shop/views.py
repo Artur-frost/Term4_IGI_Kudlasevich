cff792b4d4cfb0e6f79ce59a4a4c3c4f1ec9fbb5
Artur <artur.kudlasewitsch@gmail.com>
2023-09-15 02:40:46 +0300

    add views

--- Lab4/myshop/shop/views.py
+++ Lab4/myshop/shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from cart.forms import CartAddProductForm
 from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
+from .models import Category, Product, Article, Partner, Advertisement, CompanyInfo, Contact
 
 
 def product_list(request, category_slug=None):
@@ -17,13 +17,94 @@ def product_list(request, category_slug=None):
                    'categories': categories,
                    'products': products})
 
+
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,
                                 id=id,
                                 slug=slug,
                                 available=True)
-
     cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html',
                   {'product': product, 'cart_product_form': cart_product_form})
+
+
+def home(request):
+    # Получите последнюю опубликованную статью
+    latest_article = Article.objects.latest('date_of_creation')
+
+    # Получите список компаний-партнеров
+    partners = Partner.objects.all()
+    advertisement = Advertisement.objects.first()
+
+    context = {
+        'latest_article': latest_article,
+        'partners': partners,
+        'advertisement': advertisement,  # Добавьте рекламу в контекст
+    }
+
+    return render(request, 'shop/home/home.html', context)
+
+
+def article_list(request):
+    articles = Article.objects.all()
+
+    context = {
+        'articles': articles,
+    }
+
+    return render(request, 'shop/home/article_list.html', context)
+
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'shop/home/article_detail.html', context)
+
+
+def partner_list(request):
+    partners = Partner.objects.all()
+
+    context = {
+        'partners': partners,
+    }
+
+    return render(request, 'shop/home/partner_list.html', context)
+
+
+def partner_detail(request, slug):
+    partner = get_object_or_404(Partner, slug=slug)
+
+    context = {
+        'partner': partner,
+    }
+
+    return render(request, 'shop/home/partner_detail.html', context)
+
+
+def company_info(request):
+    company_info = CompanyInfo.objects.first()
+
+    context = {
+        'company_info': company_info,
+    }
+
+    return render(request, 'shop/info/company_info.html', context)
+
+
+def contact_list(request):
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts,
+    }
+
+    return render(request, 'shop/info/contact_list.html', context)
+
+
+def privacy_policy(request):
+    return render(request, 'shop/info/privacy_policy.html')
