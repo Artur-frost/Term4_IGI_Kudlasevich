acb0c147a753dd2d5266f7d4b579d967cad7d61f
Artur <artur.kudlasewitsch@gmail.com>
2023-09-15 02:42:17 +0300

    add admin

--- Lab4/myshop/shop/admin.py
+++ Lab4/myshop/shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product,Article, Partner, Advertisement, CompanyInfo, Contact
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -14,4 +14,25 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['price', 'stock', 'available']
     prepopulated_fields = {'slug': ('name',)}
 admin.site.register(Product, ProductAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'short_argument','date_of_creation')
+admin.site.register(Article, ArticleAdmin)
+
+class PartnerAdmin(admin.ModelAdmin):
+    list_display = ('partner_name', 'description', 'link')
+    prepopulated_fields = {'slug': ('partner_name',)}
+admin.site.register(Partner, PartnerAdmin)
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('adv_name', 'link')
+admin.site.register(Advertisement,AdvertisementAdmin)
+class CompanyInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'video_url', 'logo', 'history', 'contacts', 'certificate')
+admin.site.register(CompanyInfo, CompanyInfoAdmin)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'job_title', 'phone', 'email', 'website')  # Определите, какие поля отображать в списке объектов
+    search_fields = ('first_name', 'last_name', 'job_title', 'email')  # Добавьте поля для поиска
+admin.site.register(Contact, ContactAdmin)
 
